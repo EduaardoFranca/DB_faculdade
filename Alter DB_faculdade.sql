6baa069eb11bdb369129a9b33ccc358369942f4e
Eduardo <edubsb17@gmail.com>
2022-06-08 14:09:02 -0300

    alterações de banco

---
+++ Alter DB_faculdade.sql
@@ -0,0 +1,25 @@
+ALUNO:
+ALTER TABLE aluno ADD FOREIGN KEY (fk_cod_turma) REFERENCES turma (cod_turma);
+ALTER TABLE aluno ADD FOREIGN KEY (fk_cod_curso) REFERENCES curso (cod_curso);
+ALTER TABLE aluno ADD FOREIGN KEY (fk_cod_endereco) REFERENCES endereco (cod_endereco);
+
+   
+PROFESSOR:
+
+ALTER TABLE professor ADD FOREIGN KEY (fk_cod_departamento) REFERENCES departamento (cod_departamento);
+
+CURSO
+ALTER TABLE curso ADD FOREIGN KEY (fk_cod_departamento) REFERENCES departamento (cod_departamento);
+Disciplina
+ALTER TABLE disciplina  ADD FOREIGN KEY (fk_cod_departamento) REFERENCES departamento (cod_departamento);
+Telefone
+ALTER TABLE telefone ADD FOREIGN KEY (fk_cod_tipo) REFERENCES tipo_telefone (cod_tipo);
+LOGRADOURO
+ALTER TABLE endereco ADD FOREIGN KEY (fk_cod_tipo_logradouro) REFERENCES tipo_logradouro (cod_tipo_logradouro);
+
+Telefone_aluno
+ALTER TABLE telefone_aluno ADD FOREIGN KEY (fk_cod_telefone) REFERENCES telefone (cod_telefone);
+
+Historico 
+ALTER TABLE historico ADD FOREIGN KEY (fk_RA) REFERENCES aluno (RA);
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Este projeto tem o intutito de aprender o uso do GIT e implementar o banco de dados SQL.
+<<<<<<< HEAD
 Aula DBA 
+=======
+Aula DBA 
+>>>>>>> a149d251ead0ebb88e16b80ca8bc95fcea4a2fc8
