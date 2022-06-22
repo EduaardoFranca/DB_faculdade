ea1d120e9f4ef9c40b90ac3647cade812b65b985
EduaardoFranca <106835869+EduaardoFranca@users.noreply.github.com>
2022-06-22 16:00:20 -0300

    Update Alter DB_faculdade.sql

--- Alter DB_faculdade.sql
+++ Alter DB_faculdade.sql
@@ -1,25 +1,27 @@
 ALUNO:
-ALTER TABLE aluno ADD FOREIGN KEY (fk_cod_turma) REFERENCES turma (cod_turma);
-ALTER TABLE aluno ADD FOREIGN KEY (fk_cod_curso) REFERENCES curso (cod_curso);
-ALTER TABLE aluno ADD FOREIGN KEY (fk_cod_endereco) REFERENCES endereco (cod_endereco);
+   ALTER TABLE aluno ADD FOREIGN KEY (fk_cod_turma) REFERENCES turma (cod_turma);
+   ALTER TABLE aluno ADD FOREIGN KEY (fk_cod_curso) REFERENCES curso (cod_curso);
+   ALTER TABLE aluno ADD FOREIGN KEY (fk_cod_endereco) REFERENCES endereco (cod_endereco);
 
    
 PROFESSOR:
-
-ALTER TABLE professor ADD FOREIGN KEY (fk_cod_departamento) REFERENCES departamento (cod_departamento);
+   ALTER TABLE professor ADD FOREIGN KEY (fk_cod_departamento) REFERENCES departamento (cod_departamento);
 
 CURSO
-ALTER TABLE curso ADD FOREIGN KEY (fk_cod_departamento) REFERENCES departamento (cod_departamento);
+   ALTER TABLE curso ADD FOREIGN KEY (fk_cod_departamento) REFERENCES departamento (cod_departamento);
+   
 Disciplina
-ALTER TABLE disciplina  ADD FOREIGN KEY (fk_cod_departamento) REFERENCES departamento (cod_departamento);
+   ALTER TABLE disciplina  ADD FOREIGN KEY (fk_cod_departamento) REFERENCES departamento (cod_departamento);
+   
 Telefone
-ALTER TABLE telefone ADD FOREIGN KEY (fk_cod_tipo) REFERENCES tipo_telefone (cod_tipo);
+   ALTER TABLE telefone ADD FOREIGN KEY (fk_cod_tipo) REFERENCES tipo_telefone (cod_tipo);
+   
 LOGRADOURO
-ALTER TABLE endereco ADD FOREIGN KEY (fk_cod_tipo_logradouro) REFERENCES tipo_logradouro (cod_tipo_logradouro);
+   ALTER TABLE endereco ADD FOREIGN KEY (fk_cod_tipo_logradouro) REFERENCES tipo_logradouro (cod_tipo_logradouro);
 
 Telefone_aluno
-ALTER TABLE telefone_aluno ADD FOREIGN KEY (fk_cod_telefone) REFERENCES telefone (cod_telefone);
+   ALTER TABLE telefone_aluno ADD FOREIGN KEY (fk_cod_telefone) REFERENCES telefone (cod_telefone);
 
 Historico 
-ALTER TABLE historico ADD FOREIGN KEY (fk_RA) REFERENCES aluno (RA);
+   ALTER TABLE historico ADD FOREIGN KEY (fk_RA) REFERENCES aluno (RA);
 
